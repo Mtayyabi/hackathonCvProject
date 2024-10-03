eae5d3dd267b3f9d25611016afbe8370b8c42be6
muhammadtayyab <tayyabmeo8@gmail.com>
2024-10-03 21:41:33 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+"use strict";
+function hideBtn() {
+    let button = document.getElementById("skillsHide");
+    if (button.style.display === "none") {
+        button.style.display = "block";
+    }
+    else {
+        button.style.display = "none";
+    }
+}
+;
+let hideButton = document.getElementById("hB");
+hideButton.onclick = () => {
+    let button = document.getElementById("skillsHide");
+    if (button.style.display === "none") {
+        button.style.display = "block";
+    }
+    else {
+        button.style.display = "none";
+    }
+};
